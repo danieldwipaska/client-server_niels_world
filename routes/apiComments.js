2cb27fe22a7bccd07f5def90c20fc514cf55e9e1
Daniel Kamasi <scientificallychristian@gmail.com>
2022-07-27 12:52:54 +0700

    Telegram Bot

--- routes/apiComments.js
+++ routes/apiComments.js
@@ -1,15 +1,24 @@
 const router = require('express').Router();
 const Comment = require('../models/Comment');
 
+//TELEGRAM BOT
+const telegramBot = require('node-telegram-bot-api');
+const bot = new telegramBot(process.env.TELE_TOKEN, { polling: true });
+
 //CREATE A COMMENT
 router.post('/', async (req, res) => {
   try {
     const newComment = new Comment(req.body);
     const savedComment = await newComment.save();
+
+    //TELEGRAM BOT
+    bot.sendMessage(savedComment.fullname);
     res.redirect(`/feeds/${savedComment.postSlug}`);
   } catch (err) {
     res.status(500).json(err);
   }
 });
+
+//TELEGRAM BOT
 
 module.exports = router;
