f49604b034013e2e77ecfd0b7a4975e12d8b68f1
Daniel Kamasi <scientificallychristian@gmail.com>
2022-07-27 13:39:25 +0700

    Telegram Bot 6

--- routes/apiComments.js
+++ routes/apiComments.js
@@ -7,6 +7,8 @@ router.post('/', async (req, res) => {
     const newComment = new Comment(req.body);
     const savedComment = await newComment.save();
 
+    const teleSend = await fetch(`https://api.telegram.org/bot${process.env.TELE_TOKEN}/sendMessage?chat_id=@NielsWorld_bot&text=Ada+Komentar+Masuk`);
+
     res.redirect(`/feeds/${savedComment.postSlug}`);
   } catch (err) {
     res.status(500).json(err);
